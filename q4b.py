f0952319fa70950ca1c6875be9198ecbbf52dadb
Tris Olson <tris.s.olson@gmail.com>
2020-09-13 21:35:00 -0400

    making progress on q4b

--- q4b.py
+++ q4b.py
@@ -20,15 +20,50 @@ for line in file:
     
 file.close()
 
-markedList = [0] * len(relationsList)
+taskIDs = []
+
+file = open("task_ids.txt")
+
+for line in file:
+    taskIDs = taskIDs + re.findall('[0-9]+', line)
+    
+file.close()
+
+
+markedList = [0] * len(taskIDs)
 # where 0 is unmarked, 1 is marked
 path = []
 
-#actually need to rethink graph traversal to properly solve this q
-def DFS(graph, mList, edge, marking):
-    if marking == 0:
-        marking = 1
+def graphTraversal(graph, mList, node, end, maybeList):
+    if mList[taskIDs.index(node)] == 0:
+        mList[taskIDs.index(node)] = 1
+        checkedList = []
+        # this just pops everything above stream of anything else on the list, this is the issue
         for x in range(0, len(relationsList)):
-            if relationsList[x[1]] == edge[0]:
-                DFS(graph, mList, relationsList[x], mList[x])
+            if relationsList[x][1] == node:
+                checkedList = checkedList + [relationsList[x][0], graphTraversal(graph, mList, relationsList[x][0], end, maybeList)]
+        for x in range(0, len(relationsList)):   
+            if relationsList[x][0] == node:
+                if relationsList[x][1] == end:
+                    checkedList = checkedList + [end, maybeList]
+                else:
+                    maybeList = maybeList + [relationsList[x][1]]
+                    checkedList = checkedList + [graphTraversal(graph, mList, relationsList[x][1], end, maybeList)]
+        return checkedList
+    
+def removeNone(l):
+    newList = []
+    for x in l:
+        if x != None:
+            newList = newList + [x]
+    return newList
+    
+def flatten(l):
+    flat_list = []
+    for x in l:
+        flat_list.append(x)
+    return flat_list
 
+# testing but this probably won't work right away
+emptyList = []
+print(flatten(removeNone(graphTraversal(relationsList, markedList, '73', '36', emptyList))))
