7d5d2a46c69e205a83361cf04fce31ef58339952
Tris Olson <tris.s.olson@gmail.com>
2020-09-14 15:58:20 -0400

    correct solution for q4b
    still need to add comments and tidy up code, but I'm pretty confident this solution is actually correct

--- q4b.py
+++ q4b.py
@@ -1,12 +1,8 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sun Sep 13 19:50:49 2020
-
 just doing a quick lil graph traversal
 start node: 73
 end node: 36
 
-@author: Tricia
 """
 
 import re
@@ -31,41 +27,20 @@ for line in file:
 file.close()
 
 
-def graphTraversal(goal, edges, seenList):
-    path = ['0']
-    if seenList[taskIDs.index(goal)] == 0:
-        seenList[taskIDs.index(goal)] = 1
-        path.append(goal)
+def graphTraversal(goal, edges, visited, stack):
+    if visited[taskIDs.index(goal)] == 0:
+        visited[taskIDs.index(goal)] = 1
         for x in range(0, len(edges)):
             if edges[x][1] == goal:
-                path.append(graphTraversal(edges[x][0], edges, seenList))
-    return path
-    
-
-def flatten(l):
-    newList = []
-    for x in l:
-        if isinstance(l, collections.Iterable) and not isinstance(x, str):
-            newList.extend(flatten(x))
-        else:
-            newList.append(x)
-    return newList
-
-def removeZeros(l):
-    newList = []
-    for x in l:
-        if x != '0':
-            newList.append(x)
-    return newList
+                graphTraversal(edges[x][0], edges, visited, stack)
+        stack.append(goal)
+    return stack
 
 def findPath(start, goal, nodes, edges):
-    markedList = [0] * len(nodes)
-    # where 0 is unmarked, 1 is marked
-    goalDependencies = graphTraversal(goal, edges, markedList)
-    goalDependencies = removeZeros(flatten(goalDependencies))
-    markedList = [0] * len(nodes)
-    startDependencies = graphTraversal(start, edges, markedList)
-    startDependencies = removeZeros(flatten(startDependencies))
+    visitedList = [0] * len(nodes)  # where 0 is unvisited, 1 is visited
+    goalDependencies = graphTraversal(goal, edges, visitedList, [])
+    visitedList = [0] * len(nodes)
+    startDependencies = graphTraversal(start, edges, visitedList, [])
     
     finalPath = []
     for x in goalDependencies:
@@ -73,26 +48,7 @@ def findPath(start, goal, nodes, edges):
             finalPath.append(x)
         if x == start:
             finalPath.append(x)
-    
     return finalPath
 
     
 print(findPath('73', '36', taskIDs, relationsList))
-
-                
-
-"""
-    
-def removeNone(l):
-    newList = []
-    for x in l:
-        if x != None:
-            newList = newList + [x]
-    return newList
-    
-def flatten(l):
-    flat_list = []
-    for x in l:
-        flat_list.append(x)
-    return flat_list
-"""
