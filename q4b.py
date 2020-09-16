a829d9383d2db379dab17c933a1782a56deb38c9
Tris Olson <tris.s.olson@gmail.com>
2020-09-15 22:15:53 -0400

    small edits to naming

--- q4b.py
+++ q4b.py
@@ -6,9 +6,9 @@ import re
 
 # load in list of edges
 file = open("relations.txt")  
-edgesList = []
+edges_list = []
 for line in file:
-    edgesList = edgesList + [re.findall('[0-9]+', line)]  
+    edges_list = edges_list + [re.findall('[0-9]+', line)]  
 file.close()
 
 # load in task list
@@ -19,32 +19,32 @@ for line in file:
 file.close()
 
 # finds a topological sorting for the graph above the goal node
-def graphTraversal(goal, nodes, edges, visited, stack):
+def graph_traversal(goal, nodes, edges, visited, stack):
     if visited[nodes.index(goal)] == 0:  # if current node hasn't been visited yet
         visited[nodes.index(goal)] = 1   # update visited status
         for x in range(0, len(edges)):
             if edges[x][1] == goal:      # for all prereq nodes connected to current node
                 # recursively call graphTraversal to find all earlier prereq nodes
-                graphTraversal(edges[x][0], nodes, edges, visited, stack)
+                graph_traversal(edges[x][0], nodes, edges, visited, stack)
         # appending to stack here preserves topological sorting
         stack.append(goal)
     return stack
 
 # computes the target path taking into account start and goal nodes
-def findPath(start, goal, nodes, edges):
-    visitedList = [0] * len(nodes)  # where 0 is unvisited, 1 is visited
-    goalDependencies = graphTraversal(goal, nodes, edges, visitedList, [])
-    visitedList = [0] * len(nodes)
-    startDependencies = graphTraversal(start, nodes, edges, visitedList, [])
+def find_path(start, goal, nodes, edges):
+    visited_list = [0] * len(nodes)  # where 0 is unvisited, 1 is visited
+    goal_dependencies = graph_traversal(goal, nodes, edges, visited_list, [])
+    visited_list = [0] * len(nodes)
+    start_dependencies = graph_traversal(start, nodes, edges, visited_list, [])
     
     # remove any prereqs that have been already satisfied given the start node
-    finalPath = []
-    for x in goalDependencies:
-        if x not in startDependencies:  # remove any prereqs already satisfied
-            finalPath.append(x)         # given the start node
+    final_path = []
+    for x in goal_dependencies:
+        if x not in start_dependencies:  # remove any prereqs already satisfied
+            final_path.append(x)         # given the start node
         if x == start:                  
-            finalPath.append(x)         # keep start node if it is a prereq of the goal
-    return finalPath
+            final_path.append(x)         # keep start node if it is a prereq of the goal
+    return final_path
 
     
-print(findPath('73', '36', taskIDs, edgesList))
+print(find_path('73', '36', taskIDs, edges_list))
