2b93ffd0bbc71e08faae426ebdd3b90a7d1afd6d
Tris Olson <tris.s.olson@gmail.com>
2020-09-17 15:57:46 -0400

    final upload
    includes final file with code, as well as copies of the provided files I was using for testing

--- submission.py
+++ submission.py
@@ -16,7 +16,7 @@ spark = SparkSession.builder.master("local[*]") \
                     .getOrCreate()
     
 # to prevent error when writing to file       
-spark.conf.set("spark.sql.crossJoin.enabled", "true")
+spark.conf.set("spark.sql.crossJoin.enabled", 'true')
 
 ##############
 # QUESTION 1 #
@@ -29,13 +29,13 @@ df_data = spark.read.options(header='True',inferSchema='True',delimiter=',') \
 df_data = df_data.dropDuplicates(['TimeSt','Country','Province','City','Latitude','Longitude'])
 
 # filter out implausible points via a box bounding Canada's extremes
-df_data = df_data.filter(df_data["Latitude"]>41.681389)
-df_data = df_data.filter(df_data["Latitude"]<83.111389)
-df_data = df_data.filter(df_data["Longitude"]>-141.001944)
-df_data = df_data.filter(df_data["Longitude"]<-52.619444)
+df_data = df_data.filter(df_data['Latitude'] > 41.681389)
+df_data = df_data.filter(df_data['Latitude'] < 83.111389)
+df_data = df_data.filter(df_data['Longitude'] > -141.001944)
+df_data = df_data.filter(df_data['Longitude'] < -52.619444)
 
 # output for question 1
-df_data.write.option("header",True) \
+df_data.write.option('header',True) \
              .csv("/tmp/data/cleaned-data")
              
 ##############
@@ -53,8 +53,8 @@ df_pois = spark.read.options(header='True',inferSchema='True',delimiter=',') \
 df_pois = df_pois.dropDuplicates(['POILatitude','POILongitude'])
 
 # redefining geopy distance function so it's usable in dataframe
-def dist(a,b,x,y):
-    return distance.distance((a,b),(x,y)).km
+def dist(a, b, x, y):
+    return distance.distance((a, b),(x, y)).km
 udf_dist = udf(dist, FloatType())
 
 # create a new dataframe of size count(POIs)*requests, 
@@ -66,12 +66,12 @@ min_distances = distances.groupBy('_ID').min('Distance')
 # annotate distances with min distances, then filter out all non-min POIs
 # and delete unnecessary columns
 distances = distances.join(min_distances, distances._ID == min_distances._ID) \
-                     .select(distances["*"],min_distances["min(Distance)"])
-distances = distances.filter(distances["Distance"]==distances["min(Distance)"])
-df_data = distances.drop("min(Distance)", "POILatitude", "POILongitude")
+                     .select(distances['*'],min_distances['min(Distance)'])
+distances = distances.filter(distances['Distance'] == distances['min(Distance)'])
+df_data = distances.drop('min(Distance)','POILatitude','POILongitude')
 
 # output for question 2
-df_data.write.option("header",True) \
+df_data.write.option('header',True) \
              .csv("/tmp/data/assigned-data")
              
 ##############
@@ -88,22 +88,26 @@ pmean = df_data.groupBy('POIID').agg({'Distance': 'mean'})
 pstddev = df_data.groupBy('POIID').agg({'Distance': 'stddev'})
 pmax = df_data.groupBy('POIID').agg({'Distance': 'max'})
 # loading stats into new dataframe
-poi_stats = df_pois.join(pcount, df_pois.POIID == pcount.POIID).select(df_pois["*"],pcount["count"])
-poi_stats = poi_stats.join(pmean, poi_stats.POIID == pmean.POIID).select(poi_stats["*"],pmean["avg(Distance)"])
-poi_stats = poi_stats.join(pstddev, poi_stats.POIID == pstddev.POIID).select(poi_stats["*"],pstddev["stddev(Distance)"])
-poi_stats = poi_stats.join(pmax, poi_stats.POIID == pmax.POIID).select(poi_stats["*"],pmax["max(Distance)"])
+poi_stats = df_pois.join(pcount, df_pois.POIID == pcount.POIID) \
+                   .select(df_pois['*'], pcount['count'])
+poi_stats = poi_stats.join(pmean, poi_stats.POIID == pmean.POIID) \
+                     .select(poi_stats['*'], pmean['avg(Distance)'])
+poi_stats = poi_stats.join(pstddev, poi_stats.POIID == pstddev.POIID) \
+                     .select(poi_stats['*'], pstddev['stddev(Distance)'])
+poi_stats = poi_stats.join(pmax, poi_stats.POIID == pmax.POIID) \
+                     .select(poi_stats['*'], pmax['max(Distance)'])
 # computing density column with statistics
-poi_stats = poi_stats.withColumn("Density", udf_density(poi_stats["count"], poi_stats["max(Distance)"]))
+poi_stats = poi_stats.withColumn('Density', udf_density(poi_stats['count'], poi_stats['max(Distance)']))
 
 # ouput for question 3, commented out line produces a single .csv file
-# poi_stats.repartition(1).write.option("header",True).csv("/tmp/data/poi-stats", sep=',')
-poi_stats.write.option("header",True) \
+# poi_stats.repartition(1).write.option('header',True).csv("/tmp/data/poi-stats", sep=',')
+poi_stats.write.option('header',True) \
               .csv("/tmp/data/poi-stats")
 
 # read in POI statistics file produced earlier
 # reading in file speeds up execution compared to 
 # using the loaded data frame for some reason
-df_stats = spark.read.format("csv") \
+df_stats = spark.read.format('csv') \
                 .options(header='True',inferSchema='True',delimiter=',') \
                 .load("/tmp/data/poi-stats/*.csv")
 
@@ -114,7 +118,7 @@ poi_map = folium.Map(location=[57.4, -96.466667], \
 # iterate over each POI mapping desired features
 for f in df_stats.collect(): 
     folium.Circle(
-    radius=f["max(Distance)"]*1000.0, # convert from km to m
+    radius=f['max(Distance)']*1000.0, # convert from km to m
     location=[f.POILatitude, f.POILongitude],
     popup=f.POIID,
     color='crimson',
@@ -135,33 +139,37 @@ def standardize(x, mean, sd):
 udf_standardize = udf(standardize, FloatType())
 
 # rescales z-scores to [-10, 10] scale, with extreme ends
-# of the scale mapped to 3+ standard deviations from the mean
+# of the scale locked to 2+ standard deviations from the mean
 # this gives better separation to the majority of the data and
 # implies that any point assigned -10 or 10 is a significant outlier
 def rescale(pop):
-    r = -10.0+(((pop+3.0)*20.0)/(6.0))
-    if r < -10.0:
-        r = -10.0
+    r = (-10.0) + (((pop + 2.0)*20.0) / (4.0))
     if r > 10.0:
         r = 10.0
+    if r < -10.0:
+        r = -10.0
     return r
 udf_rescale = udf(rescale, FloatType())
 
 # create mean and standard deviation columns for computations
-temp = df_stats.agg({'Density':'mean'})
-df_stats = df_stats.join(temp).select(df_stats["*"],temp["avg(Density)"])
-temp = df_stats.agg({'Density':'stddev'})
-df_stats = df_stats.join(temp).select(df_stats["*"],temp["stddev(Density)"])
+cmean = df_stats.agg({'Density':'mean'})
+cstddev = df_stats.agg({'Density':'stddev'})
+# add to dataframe
+df_stats = df_stats.join(cmean).select(df_stats['*'], cmean['avg(Density)'])
+df_stats = df_stats.join(cstddev).select(df_stats['*'], cstddev['stddev(Density)'])
 # compute simple z-score
-df_stats = df_stats.withColumn("pop", udf_standardize(df_stats["Density"], df_stats["avg(Density)"], df_stats["stddev(Density)"]))
+df_stats = df_stats.withColumn('pop', udf_standardize(df_stats['Density'], \
+                                                      df_stats['avg(Density)'], \
+                                                      df_stats['stddev(Density)']))
 # rescale z-score
-df_stats = df_stats.withColumn("Popularity", udf_rescale(df_stats["pop"]))
+df_stats = df_stats.withColumn('Popularity', udf_rescale(df_stats['pop']))
 # remove columns used for computations
-df_stats = df_stats.drop("avg(Density)","stddev(Density)","pop")
+df_stats = df_stats.drop('avg(Density)','stddev(Density)','pop')
 
 # output for question 4a, forced into a single file
-df_stats.repartition(1).write.option("header",True).csv("/tmp/data/final-stats", sep=',')
-#df_stats.write.option("header",True) \
+df_stats.repartition(1).write.option('header',True) \
+                             .csv("/tmp/data/final-stats", sep=',')
+#df_stats.write.option('header',True) \
 #              .csv("/tmp/data/final-stats")
 
 ###############
@@ -188,6 +196,8 @@ file = open("/tmp/data/question.txt")
 for line in file:
     question = question + re.findall('[0-9]+', line)   
 file.close()
+start = question[0]
+goal = question[1]
 
 # finds a topological sorting for the graph above the goal node
 def graph_traversal(goal, nodes, edges, visited, stack):
@@ -218,7 +228,7 @@ def find_path(start, goal, nodes, edges):
     return final_path
   
 # output for question 4b
-finalString = ', '.join(find_path(question[0], question[1], taskIDs, edges_list))
-file = open("/tmp/data/pipeline-answer.txt","w")
+finalString = ', '.join(find_path(start, goal, taskIDs, edges_list))
+file = open("/tmp/data/pipeline-answer.txt",'w')
 file.write(finalString)
 file.close() 
